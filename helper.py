2c24b44b809a885abd09e217f50e6bc31a775be6
Lasse Bergmann <lasse.bergmann@artefact.com>
2023-06-07 11:06:24 +0200

    fix

--- helper.py
+++ helper.py
@@ -19,16 +19,15 @@ class Printer:
         self.count_dict = {}
 
     def save_data(self, line: dict):
-        line = clean(line)
         match = self.count_dict.get(line['car_model'])
         if match is True:
             self.writer.writerow(line)
         elif match is None:
             self.count_dict[line['car_model']] = {"count": 1, "queue": [line]}
-        elif match['count'] < 3:
+        elif match['count'] < 2:
             self.count_dict[line['car_model']]["count"] += 1
             self.count_dict[line["car_model"]]["queue"].append(line)
-        elif match['count'] == 3:
+        elif match['count'] == 2:
             for line in self.count_dict[line["car_model"]]["queue"]:
                 self.writer.writerow(line)
             self.writer.writerow(line)
@@ -48,18 +47,18 @@ def process_csv(path: str):
         for line in csv.reader(csvfile):
             if line[0] == 'car_model':
                 continue
-            printer.save_data({col_names[i]: line[i] for i in range(len(col_names))})
+            printer.save_data(clean({col_names[i]: line[i] for i in range(len(col_names))}))
 
 
 def process_json(path: str):
     with open(path) as jsonfile:
         for line in jsonfile:
-            printer.save_data(dict(json.loads(line)))
+            printer.save_data(clean(dict(json.loads(line))))
 
 
 def process_xml(path: str):
     for child in etree.parse(path).getroot():
-        printer.save_data({x.tag: x.text for x in child})
+        printer.save_data(clean({x.tag: x.text for x in child}))
 
 
 def list_dir(path: str) -> list:
